e0d6a5fd56efbce1986ef3af4d61e5b16a8b6218
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-10-20 23:31:05 +0900

    productionモードでのビルド時にconsole.error以外は除く

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const terserPlugin = require('terser-webpack-plugin')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 const eslintWebpackPlugin = require('eslint-webpack-plugin')
 
@@ -34,6 +35,18 @@ module.exports = {
         ],
       },
     ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new terserPlugin({
+      terserOptions: {
+        compress: {
+          // 次のものはproductionモードのときに除かれる
+          pure_funcs: ['console.log', 'console.info', 'console.warn', 'console.debug'],
+        },
+      },
+      extractComments: 'all',
+    })],
   },
   plugins: [
     new htmlWebpackPlugin({
