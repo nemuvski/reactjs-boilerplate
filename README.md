b88bad32f42b78c18714f5652a2a74f9c21f227f
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-10-22 18:44:02 +0900

    dotenvファイル, アプリバージョンを含めるようにカスタマイズ

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 # React.js Boilerplate
 
+🚧 あくまで個人用のReact.jsプロジェクトのたたき台です。
+
+プロジェクトの構成の概要です。
+
+- React.js
 - TypeScript
 - ESLint
 - Prettier
+- Webpack
+
+ビルド設定のカスタマイズは `webpack/webpack.config.cjs` でご参照ください。
+
+## コマンド
+
+- `yarn start`
+  - `localhost:3000` でローカル環境が立ち上がります。
+- `yarn build`
+  - 問題なくビルドできれば、 `dist` ディレクトリが作成されます。
+
+詳しくは `package.json` をご参照ください。
+
+## `.env` ファイル
+
+読み込むファイルは以下のパターンです。
+
+- `.env.development` ファイル
+  - `yarn start` コマンドを実行したときに読み込まれます。
+- `.env.production` ファイル
+  - `yarn build` コマンドを実行したときに読み込まれます。
--- webpack/webpack.config.cjs
+++ webpack/webpack.config.cjs
@@ -1,72 +1,94 @@
+const webpack = require('webpack')
 const path = require('path')
+const dotenv = require('dotenv-webpack')
 const terserPlugin = require('terser-webpack-plugin')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 const eslintWebpackPlugin = require('eslint-webpack-plugin')
 const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const headerMessage = require('./scripts/header-message.cjs')
 
 const ROOT_DIR_PATH = path.resolve(__dirname, '..')
 const ENTRY_FILENAME = 'index.tsx'
 const OUTPUT_DIRNAME = 'dist'
 const DEV_SERVER_PORT = 3000
 
-module.exports = {
-  entry: path.resolve(ROOT_DIR_PATH, 'src', ENTRY_FILENAME),
-  resolve: {
-    modules: [path.resolve(ROOT_DIR_PATH, 'node_modules')],
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    alias: {
-      '~': path.resolve(ROOT_DIR_PATH, 'src'),
+module.exports = (env, argv) => {
+  const mode = argv.mode
+  const isProductionMode = mode === 'production'
+  const appVersion = isProductionMode ? env.version : `${env.version}-${mode}`
+
+  headerMessage(mode, appVersion)
+
+  return {
+    entry: path.resolve(ROOT_DIR_PATH, 'src', ENTRY_FILENAME),
+    resolve: {
+      modules: [path.resolve(ROOT_DIR_PATH, 'node_modules')],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      alias: {
+        '~': path.resolve(ROOT_DIR_PATH, 'src'),
+      },
     },
-  },
-  output: {
-    path: path.join(ROOT_DIR_PATH, OUTPUT_DIRNAME),
-    filename: '[name].[contenthash].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[jt]sx?$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+    output: {
+      path: path.join(ROOT_DIR_PATH, OUTPUT_DIRNAME),
+      filename: '[name].[contenthash].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.[jt]sx?$/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: [miniCssExtractPlugin.loader, 'css-loader'],
+        },
+      ],
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new terserPlugin({
+          terserOptions: {
+            compress: {
+              // 次のものはproductionモードのときに除かれる
+              pure_funcs: ['console.log', 'console.info', 'console.warn', 'console.debug'],
             },
           },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [miniCssExtractPlugin.loader, 'css-loader'],
-      },
-    ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new terserPlugin({
-        terserOptions: {
-          compress: {
-            // 次のものはproductionモードのときに除かれる
-            pure_funcs: ['console.log', 'console.info', 'console.warn', 'console.debug'],
-          },
-        },
-        extractComments: 'all',
+          extractComments: 'all',
+        }),
+      ],
+    },
+    plugins: [
+      new webpack.EnvironmentPlugin({
+        APP_VERSION: appVersion,
+      }),
+      new dotenv({
+        path: path.join(ROOT_DIR_PATH, `.env.${mode}`),
+        ignoreStub: true,
+        // ファイルがない場合を考慮
+        silent: true,
+      }),
+      new htmlWebpackPlugin({
+        template: path.resolve(ROOT_DIR_PATH, 'src', 'index.html'),
+        // テンプレートで利用する変数
+        APP_VERSION: appVersion,
+      }),
+      new eslintWebpackPlugin({
+        extensions: ['ts', 'tsx', 'js', 'jsx'],
+      }),
+      new miniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
       }),
     ],
-  },
-  plugins: [
-    new htmlWebpackPlugin({
-      template: path.resolve(ROOT_DIR_PATH, 'src', 'index.html'),
-    }),
-    new eslintWebpackPlugin({
-      extensions: ['ts', 'tsx', 'js', 'jsx'],
-    }),
-    new miniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-    }),
-  ],
-  devServer: {
-    port: DEV_SERVER_PORT,
-  },
+    devServer: {
+      port: DEV_SERVER_PORT,
+    },
+  }
 }
