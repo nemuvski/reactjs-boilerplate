645b53a6900001c0eda57ca0d281499bc1cb286a
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-11-27 01:10:20 +0900

    Vite導入, 設定ファイルのベースを用意等

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const BASE_PATH = '/'
+const CLIENT_PUBLIC_ENV_PREFIX = 'PUBLIC_'
+const ROOT_DIR_PATH = resolve(__dirname, 'src')
+const PUBLIC_ASSETS_DIR_PATH = resolve(__dirname, 'static')
+const OUTPUT_DIR_PATH = resolve(__dirname, 'dist')
+const ENV_DIR_PATH = __dirname
+
+export default defineConfig(() => {
+  return {
+    root: ROOT_DIR_PATH,
+    publicDir: PUBLIC_ASSETS_DIR_PATH,
+    envDir: ENV_DIR_PATH,
+    envPrefix: CLIENT_PUBLIC_ENV_PREFIX,
+    base: BASE_PATH,
+    build: {
+      outDir: OUTPUT_DIR_PATH,
+      emptyOutDir: true,
+      manifest: true,
+    },
+    plugins: [react()],
+    resolve: {
+      alias: [{ find: '~', replacement: ROOT_DIR_PATH }],
+    },
+  }
+})
