345f0342fe41596aca5246eadb9e924c9db83f9d
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-08-24 18:14:14 +0900

    RCの定義を削除

--- src/types/global.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * RC (React Component)
- */
-declare namespace RC {
-  type WithChildren<Props = object> = React.FC<React.PropsWithChildren<Props>>
-  type WithoutChildren<Props = object> = React.FC<Props>
-}
