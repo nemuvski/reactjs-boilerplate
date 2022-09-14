bdc2f0ff6483d091b0de4858d44819e879030ab3
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-09-14 16:20:53 +0900

    React.StrictMode忘れ

--- src/index.tsx
+++ src/index.tsx
@@ -7,4 +7,8 @@ const container = document.getElementById('app')
 if (!container) throw new Error('Oops, root element is missing.')
 
 const root = createRoot(container)
-root.render(<App />)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
